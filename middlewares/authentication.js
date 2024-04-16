2939395478b74d36d3563c7c26ca8c7c8f3fcb92
DhirenKirpalani <dhirenkirpalani2308@gmail.com>
2024-04-16 19:37:18 +0700

    chore: create register and login

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,1 +1,27 @@
+const User = require("../models/user");
+const {verify} = require("../helpers/jwt");
 
+async function authentication(req, res, next) {
+    try {
+        const {authorization} = req.headers;
+        if(!authorization) {
+            throw {name: "Unauthenticated"}
+        }
+        const [type, token] = authorization.split(" ")
+        if(type !== "Bearer") {
+            throw {name: "Unauthenticated"}
+        }
+        const payload = verify(token)
+        const user = await User.findById(payload._id)
+        if(!user) {
+            throw {name: "Unauthenticated"}
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
+
+module.exports = authentication;
