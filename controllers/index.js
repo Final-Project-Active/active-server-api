82561b95a7a2ccab2be72a0b5ec46a5306fbe468
DhirenKirpalani <dhirenkirpalani2308@gmail.com>
2024-04-17 20:25:09 +0700

    chore: create analytics, user, workout, userworkout endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,8 @@
 const Article = require('./articleController')
 const User = require("./userController")
+const Analytics = require("./analyticsController")
+const Workout = require("./workoutController")
+const UserWorkout = require("./userworkoutController")
 
 const home = (req, res) => {
   res.status(200).json({ message: 'Welcome to Active Server API' })
@@ -9,5 +12,12 @@ module.exports = {
   home,
   getArticles: Article.getArticles,
   registerUser: User.registerUser,
-  loginUser: User.loginUser
+  loginUser: User.loginUser,
+  findById: User.findById,
+  createAnalytics: Analytics.createAnalytics,
+  getAnalytics: Analytics.getAnalytics,
+  getWorkouts: Workout.getWorkouts,
+  getWorkoutById: Workout.getWorkoutById,
+  addUserWorkout: UserWorkout.addUserWorkout,
+  updateUserWorkout: UserWorkout.updateUserWorkout
 }
--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Controller = require("../controllers");
+const authentication = require("../middlewares/authentication");
 
 const router = express.Router();
 
@@ -7,5 +8,12 @@ router.get('/', Controller.home);
 router.get('/articles', Controller.getArticles);
 router.post("/register", Controller.registerUser);
 router.post("/login", Controller.loginUser);
+router.get("/user", authentication, Controller.findById);
+router.post("/analytics", authentication, Controller.createAnalytics);
+router.get("/analytics", authentication, Controller.getAnalytics);
+router.get("/workout", authentication, Controller.getWorkouts);
+router.get("/workout/:workoutId", authentication, Controller.getWorkoutById);
+router.post("/userworkout/:workoutId", authentication, Controller.addUserWorkout);
+router.put("/userworkout/:workoutId", authentication, Controller.updateUserWorkout);
 
 module.exports = router;
